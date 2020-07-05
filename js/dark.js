1971086e78bc7c5c0f07461fd7e62017263c1d03
VAJRESH <vajresh005@gmail.com>
2020-07-06 02:23:44 +0530

    Update dark.js

--- js/dark.js
+++ js/dark.js
@@ -1,8 +1,15 @@
+// Accessing DOM
 let btn = document.querySelector('#btn');
 let theme = document.querySelector('#theme');
+// Event after clicking on label
 btn.addEventListener("click",function(){
+    //parameter checks which stylesheet is applied
     if(theme.getAttribute("href") == "css/light.css"){
+        // if true then changes to dark mode on clicking
         theme.href = "css/dark.css";
     }
-    else{ theme.href = "css/light.css"; }
+    else{
+        // If false then changes to normal mode
+        theme.href = "css/light.css"; 
+    }
 })
